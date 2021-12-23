27bad66a08292cef788081dbfb34d2bf47a61348
aya.bentaarit <taarit.aya@gmail.com>
2021-12-23 21:13:23 +0100

    the import file and show users component

---
+++ src/components/File.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <label for="file" class="form-label"> Importer an Excel File </label>
+      <input class="form-control form-control-lg" id="file" type="file" @change="onChange" />
+    </div>
+    <div>
+        {{users}}
+    </div>
+  </div>
+</template>
+<script>
+import readXlsxFile from 'read-excel-file'
+export default {
+  name: "File",
+  methods: {
+  onChange(event) {
+    let xlsxfile = event.target.files ? event.target.files[0] : null;
+    readXlsxFile(xlsxfile).then((rows) => {
+      this.users = rows
+    })
+  }},
+  props: {
+      users : []
+      //array of arrays, 
+      //each element is a user, 
+      //except for the first row (columns name)
+  },
+};
+</script>
+<style>
+</style>
