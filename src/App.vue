85b0a7bbb14445fd5effd4faa3c70b6f1d17d0bf
aya.bentaarit <taarit.aya@gmail.com>
2021-12-23 16:33:40 +0100

    initiliaze my vue project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My technical test"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,8 +20,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 60px;
+  margin-right: 200px;
 }
 </style>
