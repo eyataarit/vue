3008a17bc39c6cafec7b5553807b771061db63b7
aya.bentaarit <taarit.aya@gmail.com>
2021-12-23 16:34:33 +0100

    create home component

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1> Welcome dear visitor ! </h1>
+        <div>
+        You didn't sign up yet ? 
+        <router-link :to="{path: 'signup'}" >Join Us </router-link>  
+        </div>
+
+        <div>
+        You already have an account ? 
+        <router-link :to="{path: 'login'}" >Login </router-link> 
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:"Home",
+    props:{
+        msg:String
+    },
+};
+</script>
+<style>
+</style>
